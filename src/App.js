766fd6b2ec53d30fd9e33e7e2ed637f5f617bc52
viadel13 <shababizetre@gmail.com>
2023-01-09 01:12:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   
   return (
     <div className='vh-100 gradient-custom'>
-      <div className='container py-5 h-100'>
+      <div className='container  py-5 h-100'>
         <div className='row d-flex justify-content-center align-items-center h-100'>
           <div className='col col-xl-10'>
             {
